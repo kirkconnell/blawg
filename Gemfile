a8bba4b1fe8d64b057980a632c3189c98518a57a
kirkconnell <carlos.kirkconnell@gmail.com>
2011-10-05 17:04:24 -0600

    First commit

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0'
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails', "  ~> 3.1.0"
+  gem 'coffee-rails', "~> 3.1.0"
+  gem 'uglifier'
+end
+
+gem 'jquery-rails'
+gem 'unicorn'
+
+group :development, :test do
+  gem 'ruby-debug19', :require => 'ruby-debug'
+  gem 'rspec-rails', "~> 2.6"
+end
+
+group :test do
+  gem 'capybara'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blawg::Application.config.secret_token = 'f32b236e60b10db32344e2140d2d9fee6ff5e413a84cf6da84c5b794358609d9221425c837e7c8355ee90e94254d30b18470c620d7ba10bfb21ecf725a3ea81e'
